17c26454a8213bc8c331d536dfc61f4d7d0d8efe
ferenctorok <ferike.trk@gmail.com>
2021-01-18 17:21:44 +0100

    added python package files and readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# Local Minima Free Potential Field Based Path Planner
+
+## Install environment
+conda env create --prefix conda_env -f environment.yml
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+install_requires = [
+    "numpy",
+    "jupyter",
+    "matplotlib"
+]
+
+setup(
+    name="gradplanner",
+    version="0.0.1",
+    author="Ferenc Török",
+    author_email="ferike.trk@gmail.com",
+    packages=find_packages("."),
+    licence="LICENCE.txt",
+    url="https://github.com/ferenctorok/potential_field_planner",
+    install_requires=install_requires,
+)
